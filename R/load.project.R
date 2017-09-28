8a9a8e07669b562a3105696513128c9ae41c70be
kmcconeghy <kevinmcconeghy@gmail.com>
2017-09-28 13:28:26 -0400

    changes to make more automatic

--- R/load.project.R
+++ R/load.project.R
@@ -1,34 +1,49 @@
 #' @title load.project
 #'
-#' @description Initiate Project Commands
+#' @description Initiate project commands, this program will search the SrcPath directory 
+#' for files which conform to the nomenclature, A[0-9][0-9], B[0-9][0-9], C[0-9][0-9], D[0-9]
 #'
 #' @param FilePath Root folder where insheet etc. located
 #' @param INSHEET logic indicator for which files to run
 #' @param MUNGE logic indicator for which files to run
 #' @param ANALYZE logic indicator for which files to run
 #' @param REPORT logic indicator for which files to run
-#'
+#' @param SrcPath Path whether code files exist
+#' @param ReportPath Path to place log files  
+#' 
 #' @export
 #' @examples
 #'
 #' FilePath <- 'mydir'
+#' CodePath <- 'CodeDir'
 #' load.project(FilePath,INSHEET=TRUE) #Run Insheet only
 #'
 #'
 
-load.project <- function (FilePath, INSHEET = FALSE, MUNGE = FALSE, ANALYZE = FALSE,
-          REPORT = FALSE)
-{
-  if (INSHEET == TRUE) {
-    source(paste0(FilePath, "01_Insheet.R"),echo=T, print.eval=T)
+load.project <- function (FilePath, 
+                          INSHEET = F, MUNGE = F, ANALYZE = F, REPORT = F,
+                          SrcPath, ReportPath) {
+  #Generate list of code files
+    AllSrcFiles <- list.files(SrcPath, pattern='.Rmd', full.names = T)
+  
+  if (INSHEET == T) {
+    SrcFiles <- AllSrcFiles[str_detect(AllSrcFiles, 'A[0-9][0-9]')]
+    HtmlPath <- paste0(ReportPath, 'Insheet\\')
+    render2(i, HtmlPath)
   }
-  if (MUNGE == TRUE) {
-    source(paste0(FilePath, "02_Munge.R"),echo=T, print.eval=T)
-  }
-  if (ANALYZE == TRUE) {
-    source(paste0(FilePath, "03_Analyze.R"),echo=T, print.eval=T)
-  }
-  if (REPORT == TRUE) {
-    source(paste0(FilePath, "04_Report.R"),echo=T, print.eval=T)
-  }
+  if (MUNGE == T) {
+    SrcFiles <- AllSrcFiles[str_detect(AllSrcFiles, 'B[0-9][0-9]')]
+    HtmlPath <- paste0(ReportPath, 'Munge\\')
+    render2(i, HtmlPath)  
+    }
+  if (ANALYZE == T) {
+    SrcFiles <- AllSrcFiles[str_detect(AllSrcFiles, 'C[0-9][0-9]')]
+    HtmlPath <- paste0(ReportPath, 'Analyze\\')
+    render2(i, HtmlPath)  
+    }
+  if (REPORT == T) {
+    SrcFiles <- AllSrcFiles[str_detect(AllSrcFiles, 'D[0-9][0-9]')]
+    HtmlPath <- paste0(ReportPath, 'Report\\')
+    render2(i, HtmlPath)  
+    }
 }
