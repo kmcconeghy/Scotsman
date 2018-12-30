3604c8a3106e607fcd1bd429d31237b9f700af06
kmcconeghy <kevinmcconeghy@gmail.com>
2018-12-30 13:18:23 -0500

    added quotes txt and loaded, updated docs

--- data-raw/mk_FileNames.R
+++ data-raw/mk_FileNames.R
@@ -1,4 +1,3 @@
 FileNames <- readxl::read_excel('~\\GitHub\\Scotty\\data-raw\\FileNames.xlsx',
                                 col_names=FALSE)
-
-save(FileNames, file='~\\GitHub\\Scotty\\data\\FileNames.Rda')
+usethis::use_data(FileNames, overwrite=T)
---
+++ data-raw/mk_quotes.R
@@ -0,0 +1,2 @@
+Quotes <- read.csv('~\\GitHub\\Scotty\\data-raw\\Quotes.txt')
+usethis::use_data(Quotes, overwrite=T)
